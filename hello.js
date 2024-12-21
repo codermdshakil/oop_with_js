bd5fde536b5fe382b552e79c0cb5ae198f2c1450
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-22 01:51:00 +0600

    Learn : Class Vs Object

--- hello.js
+++ hello.js
@@ -1,3 +1,29 @@
 
 console.log("Hello OOP!")
 console.log("OOP is Awsome!")
+
+/** what is class ?
+ *  Class is a tamplate to creating multiples Objects
+ * 
+ * what things are in class?
+ * - Shape of data
+ * - Properties 
+ * - Attributes
+ * - Methods -> Public, Private
+ * 
+ * Class is a Blueprint to create multiple Object's
+ * 
+ * 
+ * What is Object?
+ * 
+ * => Anything to represents need  multiple attributes it called an Object
+ * - Using Class Tamplate User we can create multiple user or objects
+ * 
+ * what things are in Object?
+ * - Date 
+ * - Public Method
+ * 
+ * Object is a combination of Noun, Verb and Adjactive
+ * 
+ * 
+*/

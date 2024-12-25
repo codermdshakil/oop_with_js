d710e1bd2cfe6f4f9d9e701967f44ae5c6168c60
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-25 23:57:50 +0600

    Learn: Abstraction of OOP

--- things_are_important_for_oop.js
+++ things_are_important_for_oop.js
@@ -10,5 +10,15 @@
  * 3. Inheritance
  * 4. Polymorphism
  * 
+ *
+ * 1. Abstraction - Hide Implementation Details
  * 
+ * Example: We use MP3 player but we don't know how it's work behind the screen
+ * Example: Math.floor(), Math.ceil(), Math.sqrt(), Math.round(), Math.random(), we use these things but we don't know how it's works behind the screen 
+ * Example: var d = new Date()
+ * d.getFullYear()
+ * d.getMonth()
+ * d.getMinutes()
+ * d.getTime()
+ * we use these things but we don't know how it's works behind the screen 
 */

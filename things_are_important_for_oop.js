11e16b57b0e026a881c53b59d2f3b5185e204bdc
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-25 23:25:31 +0600

    Learn : Things that are important for OOP

---
+++ things_are_important_for_oop.js
@@ -0,0 +1,14 @@
+
+// Things that are important for OOP
+
+/**
+ * Object is the main thing for OOP but not everything
+ * 
+ * OOP Four Main Pillar's
+ * 1. Abstraction
+ * 2. Encapsulation
+ * 3. Inheritance
+ * 4. Polymorphism
+ * 
+ * 
+*/

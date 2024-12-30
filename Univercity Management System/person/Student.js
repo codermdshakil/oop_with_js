622a0b2725f91ff6bacec5e1c720d7bc590d4433
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-30 19:34:04 +0600

    Create : Student Class that inherit UniPerson

---
+++ Univercity Management System/person/Student.js
@@ -0,0 +1,60 @@
+
+const UniPerson = require('./UniPerson')
+const Guardian = require('./Guardian')
+
+const _studentId = Symbol('studentId')
+const _guardian = Symbol('guardian')
+const _exams = Symbol('exams')
+const _fee = Symbol('fee')
+
+class Student extends UniPerson {
+
+    constructor(id, name,studentId, guardian, fee) {
+        super(id, name)
+        this[_studentId] = studentId
+        this[_guardian] = guardian
+        this[_exams] = []
+        this[_fee] = fee
+    }
+
+    get studentId(){
+        return this[_studentId]
+    }
+
+    get guardian(){
+        return this[_guardian]
+    }
+
+    set guardian(value){
+        this[_guardian] = value
+    }
+
+    get exams(){
+        return this[_exams]
+    }
+
+    set exams(value){
+        this[_exams] = value
+    }
+
+    addExam(value){
+        this[_exams].push(value)
+    }
+
+    get fee(){
+        return this[_fee]
+    }
+
+    set fee(value){
+        this[_fee] = value
+    }
+
+    toString(){
+        super().toString()
+        console.log(`StudentId: ${this[_studentId]}`);
+    }
+
+
+}
+
+module.exports = Student

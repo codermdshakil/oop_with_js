471c52cb3493b3bbcb3f52e0ee1a65c1b82d815e
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-30 19:14:07 +0600

    Create : UniPerson class that inheritence Person class

---
+++ Univercity Management System/person/UniPerson.js
@@ -0,0 +1,40 @@
+
+const Person = require('./Person');
+
+const _department = Symbol('department');
+const _account = Symbol('account');
+
+
+class UniPerson extends Person{
+    constructor(id, name) {
+        super(id, name)
+        this[_department] = null;
+        this[_account] = null;
+    }
+
+    get department(){
+        return this[_department]
+    }
+
+    set department(value){
+        this[_department] = value
+    }
+
+    get account(){
+        return this[_account]
+    }
+
+    set account(value){
+        this[_account] = value
+    }
+
+    toString(){
+        super().toString();
+        console.log(`Department = ${this[_department]}, Account = ${this[_account]}`);
+        console.log(`Account = ${this[_account]}`);
+    }
+
+
+}
+
+export default UniPerson;
